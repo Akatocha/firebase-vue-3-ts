10837b6ac7486ff61b4b86ae4f23ae4a5d560ef6
Hudyma Roman <hudymaroma.intertech@gmail.com>
2022-03-30 14:41:24 +0300

    git setup

---
+++ dist/precache-manifest.844329d99f55e8bfce1f89e4f4e5d3f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f056a06834d804dfcdcf",
+    "url": "/css/app.29298ea8.css"
+  },
+  {
+    "revision": "c7e8a954925ec9f18026",
+    "url": "/css/chunk-vendors.fa0c18c4.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "25961b505c3a99486b197853d19f608e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7f6cf392beeb5ca41b7",
+    "url": "/js/about.531012c7.js"
+  },
+  {
+    "revision": "f056a06834d804dfcdcf",
+    "url": "/js/app.3d0a3b6b.js"
+  },
+  {
+    "revision": "c7e8a954925ec9f18026",
+    "url": "/js/chunk-vendors.63dd23be.js"
+  },
+  {
+    "revision": "94e54a915b2b359b04e81a12789d34ca",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
