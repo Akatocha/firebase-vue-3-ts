87bb68a6aa604158f3ab22c1dd7ff2d81780fce1
Hudyma Roman <hudymaroma.intertech@gmail.com>
2022-04-01 12:37:38 +0300

    fix

--- src/helpers/User.ts
+++ src/helpers/User.ts
@@ -1,10 +1,11 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-export const getUserState = async () => {
-  let isSingIn = false;
-  await onAuthStateChanged(getAuth(), user => {
-    isSingIn = Boolean(user);
-  });
-  return isSingIn;
+export const getUserState = () => {
+  return new Promise((res, rej)=> {
+    onAuthStateChanged(getAuth(), user => {
+      res(Boolean(user))
+    });
+
+  })
 };
 
