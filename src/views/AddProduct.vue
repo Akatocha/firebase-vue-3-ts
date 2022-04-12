8873d5c335c75e348bcaf00adfc5a00ca502b4cd
Hudyma Roman <hudymaroma.intertech@gmail.com>
2022-04-12 10:29:06 +0300

    addProduct setup mode

--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="add-product__form-box">
-    <el-form :model="productForm" :rules="rules" ref="ProductFormRef">
+    <el-form :model="productForm" :rules="rules" ref="productFormRef">
       <el-form-item prop="title">
         <el-input v-model="productForm.title" type="text" placeholder="Title" clearable />
       </el-form-item>
@@ -23,12 +23,12 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { useStore } from 'vuex';
 import { FormInstance, FormRules, ElMessage } from 'element-plus';
 import { db } from '@/firebase';
 import { push, ref, set, update, ThenableReference } from 'firebase/database';
-import { mapGetters } from 'vuex';
+import { reactive, ref as refVue } from 'vue';
 
 interface Product {
   title: string;
@@ -38,95 +38,90 @@ interface Product {
   author: string;
 }
 
-export default defineComponent({
-  data() {
-    return {
-      productForm: {
-        title: '',
-        description: '',
-        price: '',
-        count: 1,
-      },
-      rules: {
-        title: [
-          { required: true, message: 'Please input title' },
-          { min: 3, max: 50, message: 'Title must contain 3-50 characters' },
-        ],
-        description: [
-          { required: true, message: 'Please input description' },
-          { min: 3, max: 100, message: 'Description must contain 3-100 characters' },
-        ],
-        price: [
-          { required: true, message: 'Please input price' },
-          { validator: this.priceValidator, trigger: 'change' },
-        ],
-      } as FormRules,
-    };
-  },
-  computed: {
-    ...mapGetters(['GET_USER_UID', 'GET_USER_DATA']),
-    productFormRef() {
-      return this.$refs.ProductFormRef as FormInstance;
-    },
-  },
-  methods: {
-    async onSubmit(formEl: FormInstance | undefined) {
-      if (!formEl) return;
-      await formEl.validate((valid, fields) => {
-        if (valid) {
-          this.tryAddProduct();
-        } else {
-          console.log('error submit!', fields);
-        }
-      });
-    },
-    async addProductsToUser(newProductId: ThenableReference) {
-      const strKey: string = newProductId.key + ' '
-      const oldProductsStrKeys: string = this.GET_USER_DATA.products ? this.GET_USER_DATA.products : ''
-      update(ref(db, 'users/' + this.GET_USER_UID), {
-        products: oldProductsStrKeys + strKey
-      });
-    },
-    tryAddProduct() {
-      const product: Product = {
-        author: this.GET_USER_UID as string,
-        price: this.productForm.price,
-        title: this.productForm.title,
-        count: this.productForm.count,
-        description: this.productForm.description,
-      };
-      const postListRef = ref(db, 'products');
-      const newPostRef = push(postListRef);
-      set(newPostRef, product).then(() => {
-        this.addProductsToUser(newPostRef);
-        ElMessage({
-          message: 'Product Create',
-          type: 'success',
-          duration: 1000,
-          showClose: true,
-        });
-        this.cancel(this.productFormRef);
-      }).catch((error)=> {
-        console.error(error);
-      });
-    },
-    async cancel(formEl: FormInstance | undefined) {
-      if (!formEl) return;
-      this.productForm.title = '';
-      this.productForm.description = '';
-      this.productForm.price = '';
-      this.productForm.count = 1;
-      await formEl.clearValidate();
-    },
-    priceValidator(rule: any, value: any, callback: any) {
-      if (value <= 0) {
-        callback(new Error('Price should be a positive number'));
-      } else {
-        callback();
-      }
-    },
-  },
+const store = useStore();
+const productFormRef = refVue<FormInstance>();
+const productForm = reactive({
+  title: '',
+  description: '',
+  price: '',
+  count: 1,
 });
+const rules: FormRules = reactive({
+  title: [
+    { required: true, message: 'Please input title', trigger: 'blur' },
+    { min: 3, max: 50, message: 'Title must contain 3-50 characters', trigger: 'blur' },
+  ],
+  description: [
+    { required: true, message: 'Please input description', trigger: 'blur' },
+    { min: 3, max: 100, message: 'Description must contain 3-100 characters', trigger: 'blur' },
+  ],
+  price: [
+    { required: true, message: 'Please input price', trigger: 'blur' },
+    { validator: priceValidator, trigger: 'blur' },
+  ],
+});
+
+function priceValidator(rule: any, value: any, callback: any) {
+  if (value <= 0) {
+    callback(new Error('Price should be a positive number'));
+  } else {
+    callback();
+  }
+}
+
+async function onSubmit(formEl: FormInstance | undefined) {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      tryAddProduct();
+    } else {
+      console.log('error submit!', fields);
+    }
+  });
+}
+
+async function addProductsToUser(newProductId: ThenableReference) {
+  const strKey: string = newProductId.key + ' ';
+  const oldProductsStrKeys: string = store.getters.GET_USER_DATA.products ? store.getters.GET_USER_DATA.products : '';
+  update(ref(db, 'users/' + store.getters.GET_USER_UID), {
+    products: oldProductsStrKeys + strKey,
+  });
+}
+
+function tryAddProduct() {
+  const product: Product = {
+    author: store.getters.GET_USER_UID as string,
+    price: productForm.price,
+    title: productForm.title,
+    count: productForm.count,
+    description: productForm.description,
+  };
+  const postListRef = ref(db, 'products');
+  const newPostRef = push(postListRef);
+  set(newPostRef, product)
+    .then(() => {
+      addProductsToUser(newPostRef);
+      ElMessage({
+        message: 'Product Create',
+        type: 'success',
+        duration: 1000,
+        showClose: true,
+      });
+      cancel(productFormRef.value);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function cancel(formEl: FormInstance | undefined) {
+  if (!formEl) return;
+  productForm.title = '';
+  productForm.description = '';
+  productForm.price = '';
+  productForm.count = 1;
+  formEl.clearValidate();
+}
 </script>
 
 <style scoped lang="scss">
